23733c4f321f4dab27d9675b0270dce515c0e4f6
Hiroshi Yoshida <hrysd22@gmail.com>
2021-01-16 22:41:33 +0900

    Update design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,50 @@
 import Image from 'next/image';
 import styles from './index.module.scss';
 
+import Emoji from '../components/Emoji';
+
 export default function Index() {
   return (
     <main className={styles.Main}>
       <header className={styles.MainHeader}>
-        <h1>hrysd.org</h1>
+        <h1>@hrysd</h1>
       </header>
 
       <div className={styles.MainBody}>
-        <section>
+        <section className={styles.MainProfile}>
           <Image src='/icon.png' alt='icon' width={100} height={100} />
 
-          <ul className={styles.MainSns}>
+          <ul>
             <li>
-              <a href='https://twitter.com/hrysd'>Twitter</a>
+              <a href='https://twitter.com/hrysd' target='_blank' rel='noopener'>Twitter</a>
             </li>
 
             <li>
-              <a href='https://github.com/hrysd'>GitHub</a>
+              <a href='https://github.com/hrysd' target='_blank' rel='noopener'>GitHub</a>
             </li>
           </ul>
         </section>
 
-        <section>
-          <ul className={styles.MainLinks}>
+        <section className={styles.MainURLs}>
+          <p>
+            <Emoji emoji='👇' />
+            ブログとか
+            <Emoji emoji='👇' />
+          </p>
+
+          <ul>
             <li>
-              <a href='https://docs.hrysd.org'>https://docs.hrysd.org</a>
+              <Emoji emoji='✒' />
+              <a href='https://docs.hrysd.org' target='_blank' rel='noopener'>https://docs.hrysd.org</a>
             </li>
             <li>
-              <a href='https://kanda.uno'>https://kanda.uno</a>
+              <Emoji emoji='🖥' />
+              <a href='https://kanda.uno' target='_blank' rel='noopener'>https://kanda.uno</a>
             </li>
 
             <li>
-              <a href='https://kpt.hrysd.org'>https://kpt.hrysd.org</a>
+              <Emoji emoji='🖥' />
+              <a href='https://kpt.hrysd.org' target='_blank' rel='noopener'>https://kpt.hrysd.org</a>
             </li>
           </ul>
         </section>
