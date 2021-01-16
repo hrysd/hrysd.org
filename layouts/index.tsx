af8aaa8bd2470541889dffaf1dc61629fd5c4248
Hiroshi Yoshida <hrysd22@gmail.com>
2021-01-17 00:42:09 +0900

    Create layout for head elements

---
+++ layouts/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import config from '../config.js'
+
+export default function Layout({children}) {
+  return (
+    <>
+      <Head>
+        <title>{ config.title }</title>
+        <meta name='description' content={config.description} />
+        <meta name='keywords'    content={config.keywords} />
+        <meta name='theme-color' content={config.themeColor} />
+
+        <link rel='canonical' href={config.url} />
+      </Head>
+
+      {children}
+    </>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
 import '../assets/stylesheets/styles.css'
+import Layout from '../layouts'
 
 export default function HrysdOrg({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
